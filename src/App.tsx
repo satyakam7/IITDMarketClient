0c12436633314ba865d41c69115797b5905a26e9
azztt <ee3180878@iitd.ac.in>
2020-05-24 16:27:44 +0530

    added /healthz endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs
+    IonApp,
+    IonIcon,
+    IonLabel,
+    IonRouterOutlet,
+    IonTabBar,
+    IonTabButton,
+    IonTabs,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { chatboxOutline, gridOutline,homeOutline,personOutline, } from 'ionicons/icons';
+import {
+    chatboxOutline,
+    gridOutline,
+    homeOutline,
+    personOutline,
+} from 'ionicons/icons';
 import Home from './pages/home';
 import Categories from './pages/categories';
 import Chats from './pages/chats';
@@ -35,40 +40,46 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 import RegisterForm from './components/RegisterForm';
+import Health from './pages/Health';
 
 const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route path="/home" component={Home} exact={true} />
-          <Route path="/categories" component={Categories} exact={true} />
-          <Route path="/chats" component={Chats} />
-          <Route path="/myaccount" component={MyAccount} />
-          <Route path="/register" component={RegisterForm} exact />
-          <Route path="/" render={() => <Redirect to="/home" />} exact={true} />
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="home" href="/home">
-          <IonIcon size="small" icon={homeOutline}/>
-            <IonLabel>Home</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="categories" href="/categories">
-            <IonIcon size="small" icon={gridOutline} />
-            <IonLabel>Categories</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="chats" href="/chats">
-            <IonIcon size="small" icon={chatboxOutline} />
-            <IonLabel>Chats</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="myaccount" href="/myaccount">
-            <IonIcon size="small" icon={personOutline} />
-            <IonLabel>My Account</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
+    <IonApp>
+        <IonReactRouter>
+            <IonTabs>
+                <IonRouterOutlet>
+                    <Route exact path="/healthz" component={Health} />
+                    <Route path="/home" component={Home} exact />
+                    <Route path="/categories" component={Categories} exact />
+                    <Route path="/chats" component={Chats} />
+                    <Route path="/myaccount" component={MyAccount} />
+                    <Route path="/register" component={RegisterForm} exact />
+                    <Route
+                        path="/"
+                        render={() => <Redirect to="/home" />}
+                        exact
+                    />
+                </IonRouterOutlet>
+                <IonTabBar slot="bottom">
+                    <IonTabButton tab="home" href="/home">
+                        <IonIcon size="small" icon={homeOutline} />
+                        <IonLabel>Home</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab="categories" href="/categories">
+                        <IonIcon size="small" icon={gridOutline} />
+                        <IonLabel>Categories</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab="chats" href="/chats">
+                        <IonIcon size="small" icon={chatboxOutline} />
+                        <IonLabel>Chats</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab="myaccount" href="/myaccount">
+                        <IonIcon size="small" icon={personOutline} />
+                        <IonLabel>My Account</IonLabel>
+                    </IonTabButton>
+                </IonTabBar>
+            </IonTabs>
+        </IonReactRouter>
+    </IonApp>
 );
 
 export default App;
