16601318e4b120cf65df5537827b4c23315dc0ea
Satyakam Singhal <satyakam068@gmail.com>
2020-07-27 17:27:37 +0530

    sold button added

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import rootReducer from './store/reducers';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
     <Provider store={store}>
