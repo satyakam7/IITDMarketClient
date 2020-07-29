ec318d83a05971e87f6b587629c1e054e4559ceb
Satyakam Singhal <satyakam068@gmail.com>
2020-07-30 02:23:14 +0530

    logger correct

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import logger from 'react-logger';
+
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import rootReducer from './store/reducers';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
     <Provider store={store}>
