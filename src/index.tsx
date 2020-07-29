1a6b5556a3fae678f3a63632a1c8c608be7b9423
Satyakam Singhal <satyakam068@gmail.com>
2020-07-30 02:20:41 +0530

    logger

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import logger from 'react-logger';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
@@ -8,7 +8,7 @@ import rootReducer from './store/reducers';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 ReactDOM.render(
     <Provider store={store}>
